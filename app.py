1d05500a8fba590948f3b5ef321a0cf81e255a16
Pavel Sergeev <pav26051999@yandex.ru>
2023-06-23 20:46:35 +0300

    Application Interface

--- app.py
+++ app.py
@@ -34,14 +34,11 @@ class MainWindow(QMainWindow):
             "button3": self.button3.text()
         }
 
-        # Привязка функции к событию нажатия на каждую кнопку
         self.button1.clicked.connect(lambda: self.check_and_append(self.button1))
         self.button2.clicked.connect(lambda: self.check_and_append(self.button2))
         self.button3.clicked.connect(lambda: self.check_and_append(self.button3))
         self.cancel_button.clicked.connect(lambda: self.cancel())
         self.buttons = {"3": self.button1, "1": self.button2, "2": self.button3}
-        # self.label = QLabel("Waiting for data...", self)
-        # self.setCentralWidget(self.label)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key.Key_1:
@@ -60,7 +57,6 @@ class MainWindow(QMainWindow):
         self.check_and_append(self.buttons[data])
 
     def load_ui(self):
-        # Загрузка дизайна из файла .ui
         ui_file = "design.ui"
         uic.loadUi(ui_file, self)
 
@@ -76,21 +72,15 @@ class MainWindow(QMainWindow):
         self.button3.setText(self.initial_names["button3"])
 
     def check_and_append(self, clicked_button):
-        # Получение имени нажатой кнопки
         clicked_button_name = clicked_button.objectName()
-        # Получение текущих названий кнопок
         current_names = {
             "button1": self.button1.text(),
             "button2": self.button2.text(),
             "button3": self.button3.text()
         }
 
-        # Разделение имени нажатой кнопки на строки
         name_lines = current_names[clicked_button_name].split('\n')
 
-
-
-        # Если в названии кнопки остался один символ и кнопка была нажата, дописываем его в lineEdit
         if clicked_button.text().strip() == clicked_button.text() and '\n' not in clicked_button.text():
             current_text = self.lineEdit.text()
             split_current_text = current_text.split('  ')
@@ -101,7 +91,6 @@ class MainWindow(QMainWindow):
             line_edit_text = self.lineEdit.text()
             split_edit_text = line_edit_text.split('  ')
             if len(split_edit_text[-1]) == 2:
-                #############################################################################################33
                 if self.is_first_word:
                     self.is_first_word = False
                     self.matches = predict_text(split_edit_text[-1])
@@ -111,7 +100,6 @@ class MainWindow(QMainWindow):
                     context = ' '.join(map(str, split_edit_text[:len(split_edit_text) - 1]))
                     print(context)
                     self.matches = predict_text(split_edit_text[-1], context, len(split_edit_text) - 1)
-                    # self.lineEdit.text() = line_edit_text[:len(line_edit_text) - 2]
                 self.button1.setText('\n'.join(self.matches[:3]).upper())
                 self.button2.setText('\n'.join(self.matches[3:6]).upper())
                 self.button3.setText('\n'.join(self.matches[6:]).upper())
@@ -120,7 +108,6 @@ class MainWindow(QMainWindow):
                 self.button2.setText(self.initial_names["button2"])
                 self.button3.setText(self.initial_names["button3"])
 
-        # Замена первых двух пробелов на символ новой строки
         if len(name_lines) >= 3:
             new_name1 = name_lines[0].replace("  ", "\n", 2)
             new_name2 = name_lines[1].replace("  ", "\n", 2)
@@ -129,8 +116,6 @@ class MainWindow(QMainWindow):
             self.button2.setText(new_name2)
             self.button3.setText(new_name3)
 
-        # Разбиение названия кнопки button3 по двойным пробелам и запись соответствующих фрагментов в button1 и
-        # button2
         if len(name_lines) == 2:
             self.button1.setText(name_lines[0])
             self.button2.setText(name_lines[1])
